b18a0fe10eeb28d6b9d2e39e1f6b5778c39948b9
Rafał Trzop <rafaltrzop@gmail.com>
2017-03-19 20:07:15 +0100

    Add lists and trash tabs

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,21 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AuthService } from '../core/service/auth.service';
 
 @Component({
   selector: 'lists',
   styleUrls: [ './lists.component.css' ],
   templateUrl: './lists.component.html',
 })
-export class ListsComponent {
-  constructor(
-    private router: Router,
-    public authService: AuthService
-  ) { }
-
-  public logOut() {
-    this.authService.logOut();
-    this.router.navigate(['']);
-  }
-}
+export class ListsComponent { }
