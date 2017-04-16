f5d6797b08a9f5897b701dd8e263c54448fb9a77
Rafał Trzop <rafaltrzop@gmail.com>
2017-04-16 20:32:45 +0200

    Change theme-color and msapplication-TileColor

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -42,8 +42,8 @@ module.exports = {
     { rel: 'manifest', href: '/assets/manifest.json' }
   ],
   meta: [
-    { name: 'msapplication-TileColor', content: '#00bcd4' },
+    { name: 'msapplication-TileColor', content: '#673ab7' },
     { name: 'msapplication-TileImage', content: '/assets/icon/ms-icon-144x144.png', '=content': true },
-    { name: 'theme-color', content: '#00bcd4' }
+    { name: 'theme-color', content: '#673ab7' }
   ]
 };
