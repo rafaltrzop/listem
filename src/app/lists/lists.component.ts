6deb2b06186e5ca00608ced95dd100ce4539c9b1
Rafał Trzop <rafaltrzop@gmail.com>
2017-04-22 21:36:32 +0200

    Get only not soft deleted lists

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -24,7 +24,18 @@ export class ListsComponent implements OnInit {
 
   public ngOnInit() {
     this.listService.observeUserLists().subscribe((userLists) => {
-      this.userLists = userLists;
+      this.userLists = [];
+      for (let userList of userLists) {
+        userList.subscribe((list) => {
+          if (list.softDeleted) {
+            this.userLists = this.userLists.filter((item) => {
+              return item.$ref.key !== list.$key;
+            });
+          } else {
+            this.userLists.push(userList);
+          }
+        });
+      }
     });
   }
 
