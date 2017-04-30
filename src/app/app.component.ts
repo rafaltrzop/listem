9b5896b1ef709db1b74a0ac4e38cf88ade2f9cd6
Rafał Trzop <rafaltrzop@gmail.com>
2017-04-30 15:44:46 +0200

    Scroll to top on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {
   OnInit,
   ViewEncapsulation
 } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 import { AppState } from './app.service';
 
 @Component({
@@ -13,10 +15,17 @@ import { AppState } from './app.service';
 })
 export class AppComponent implements OnInit {
   constructor(
-    public appState: AppState
+    public appState: AppState,
+    private router: Router
   ) { }
 
   public ngOnInit() {
     console.log('Initial App State', this.appState.state); // TODO: remove
+
+    this.router.events.subscribe((event: NavigationEnd) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
   }
 }
