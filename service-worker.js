395dab198855f37ae5acd0233821f80ba751aa25
Rafał Trzop <rafaltrzop@gmail.com>
2017-06-03 17:34:17 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,28 @@
-var CACHE_NAME = 'listem';
+var CACHE_NAME = 'listem-v1';
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
+self.addEventListener('install', function(event) {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
         'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic',
         'https://fonts.googleapis.com/icon?family=Material+Icons'
       ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
